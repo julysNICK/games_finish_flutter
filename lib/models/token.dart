7b7ce411a2f24c5575a150be0c9a135bcd8a0500
julysNICK <julystube@gmail.com>
2023-02-23 10:44:00 -0300

    insert jwt

--- lib/models/token.dart
+++ lib/models/token.dart
@@ -1,3 +1,7 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+
+const String sharedSecret = 's3cr3t';
+
 class UserToken {
   final String token;
   final String refreshToken;
@@ -9,5 +13,45 @@ class UserToken {
       token: json['token'],
       refreshToken: json['refreshToken'],
     );
+  }
+
+  String senderCreateJwt() {
+    final payload = {
+      'token': token,
+      'refreshToken': refreshToken,
+    };
+
+    JWT jwt = JWT(payload);
+
+    return jwt.sign(SecretKey(sharedSecret),
+        expiresIn: const Duration(days: 1));
+  }
+
+  bool verifyToken(String token) {
+    try {
+      JWT.verify(token, SecretKey(sharedSecret));
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  dynamic getPayload(String token) {
+    try {
+      return JWT.tryDecode(token);
+    } catch (e) {
+      return {};
+    }
+  }
+
+  dynamic refreshJwt(String token) {
+    try {
+      final payload = getPayload(token);
+      final jwt = JWT(payload);
+      return jwt.sign(SecretKey(sharedSecret),
+          expiresIn: const Duration(days: 1));
+    } catch (e) {
+      return '';
+    }
   }
 }
